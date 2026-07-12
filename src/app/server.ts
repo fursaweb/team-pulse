85659e9e292a396dc9a4a3cfb68c90ceecb8dd82
fursaweb <fursa.web@gmail.com>
2026-07-12 13:25:24 +0300

    Prepare project for Railway deployment

--- src/app/server.ts
+++ src/app/server.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/admin", adminRouter);
 
 registerSlackHandlers();
-//startScheduler();
+startScheduler();
 
 app.get("/health", async (req, res) => {
   return res.status(200).json({ status: "OK" });
